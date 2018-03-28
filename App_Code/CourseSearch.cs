2fe7b84c99e99107c2087c4ac965423551be52ec
tylneit <36210526+tylneit@users.noreply.github.com>
2018-03-27 21:21:00 -0400

    Added search classes

---
+++ App_Code/CourseSearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;            // DataSet
+using System.Data.SqlClient;  // SqlConnection, SqlCommand, SqlDataReader
+using System.Configuration;   // to get the connection from Web.config
+using System.ComponentModel;  // DataObject, DataObjectMethod
+
+/// <summary>
+/// Summary description for CourseSearch
+/// </summary>
+public static class CourseSearch
+{
+    [DataObjectMethod(DataObjectMethodType.Select)]
+    public static SqlDataReader getCourse()
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        conn = new SqlConnection(getConnectionString());
+        conn.Open();
+
+        cmd = new SqlCommand();
+        cmd.Connection = conn;
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "SELECT * FROM Student ORDER BY CouseNumber";
+        dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+        return dr;
+    } // end of getStudent
+
+    private static string getConnectionString()
+    {
+        return ConfigurationManager.ConnectionStrings["SpiderWebConnectionString"].ConnectionString;
+    }
+}
